feaa8a86c59ab801c403ba286f45c754448102e3
mizumati.802 <mizumati.802@gmail.com>
2025-08-07 14:49:54 +0900

    default_update

--- service-worker.js
+++ service-worker.js
@@ -1,33 +1,4 @@
-const CACHE_NAME = 'redirect-product-register-cache-v1';
-const urlsToCache = [
-  '/redirect_product_register/index.html',
-  '/redirect_product_register/manifest.json',
-  '/redirect_product_register/icon-192x192.png',
-  '/redirect_product_register/icon-512x512.png'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
-});
-
+// 最小限のService Worker
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames =>
-      Promise.all(
-        cacheNames
-          .filter(cacheName => cacheName !== CACHE_NAME)
-          .map(cacheName => caches.delete(cacheName))
-      )
-    )
-  );
+  // リダイレクト専用なのでキャッシュ不要
 });
